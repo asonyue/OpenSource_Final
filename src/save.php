8caa575cd2d61f466abfe1886773602101321f01
asonyue <97880892+asonyue@users.noreply.github.com>
2023-05-22 13:04:03 +0800

    Update save.php
    some css styles are added

--- src/save.php
+++ src/save.php
@@ -1,31 +1,75 @@
 <?php
+
 $name = $_POST['name'];
 $level = $_POST['level'];
 $id = md5($name . $level);
 
 $db = new SQLite3('mydatabase.db');
 
-$sql = "INSERT INTO scores (id, name, level) VALUES ('$id', '$name', '$level')";
+$sql1 = "INSERT INTO scores (id, name, level) VALUES ('$id', '$name', '$level')";
 
-if ($db->exec($sql)) {
-    echo "Data inserted successfully";
-} else {
-    echo "Error inserting data: " . $db->lastErrorMsg();
-}
+if (!($db->exec($sql1))) {
+    error_log("Error: Insert Unsucessful");
+} 
 
-$sql = "SELECT name, level FROM scores ORDER BY level DESC LIMIT 5";
-$result = $db->query($sql);
+$sql2 = "SELECT name, level FROM scores ORDER BY level DESC LIMIT 5";
+$result = $db->query($sql2);
 
-$html = "<table>\n";
+$html = "<table class='score-table'>\n";
 $html .= "<tr><th>Name</th><th>Level</th></tr>\n";
 
+$i = 0;
 while ($row = $result->fetchArray(SQLITE3_ASSOC)) {
-    $html .= "<tr><td>" . $row['name'] . "</td><td>" . $row['level'] . "</td></tr>\n";
+    $i++;
+    if ($i % 2 == 0) {
+        $html .= "<tr class='even-row' style='background-color: #f2f2f2;'><td>" . $row['name'] . "</td><td>" . $row['level'] . "</td></tr>\n";
+    } else {
+        $html .= "<tr class='odd-row' style='background-color: #ffffff;'><td>" . $row['name'] . "</td><td>" . $row['level'] . "</td></tr>\n";
+    }
 }
 
 $html .= "</table>\n";
 
 echo $html;
 
+$sql3 = "DELETE FROM scores WHERE id NOT IN (SELECT id FROM scores ORDER BY level DESC LIMIT 5)";
+
+if (!($db->exec($sql3))) {
+    error_log("Error: Delete Unsucessful");
+}
+
 $db->close();
+
 ?>
+
+<style>
+.score-table {
+  border-collapse: collapse;
+  width: 100%;
+  margin-bottom: 20px;
+}
+
+.score-table th, .score-table td {
+  text-align: left;
+  padding: 8px;
+}
+
+.score-table th {
+  background-color: #4CAF50;
+  color: white;
+}
+
+.score-table tr.even-row {
+  background-color: #f2f2f2;
+}
+
+.score-table tr.odd-row {
+  background-color: #ffffff;
+}
+
+.score-table td {
+  font-size: 18px;
+  font-weight: bold;
+  color: #333333;
+}
+</style>
