f0d11f114b73e24b15a195da1489678754ea11b2
ProtoNeko <114766248+ProtoNeko@users.noreply.github.com>
2023-05-24 01:31:58 +0800

    Add files via upload

---
+++ src_0.2/src/game.js
@@ -0,0 +1,83 @@
+
+let buttonColours = ["red", "blue", "green", "yellow"];
+let gamePattern = [];
+let userClickedPattern= [];
+let started = false;
+let level = 0;
+
+// $(document).keypress(function() {
+//     if (!started) {
+//         $("#level-title").text("Level " + level);
+//         nextSequence();
+//         started = true;
+//     }
+// });
+
+$(window).on("keydown", function(event) {
+    if (event.keyCode === 13 && !started) {
+        const form = document.querySelector('form');
+        form.setAttribute('hidden', '');
+        $("#level-title").text("Level " + level);
+        nextSequence();
+        started = true;
+    }
+});
+
+
+$(".btn").click(function() {
+    let userChosenColor = $(this).attr("id");
+    userClickedPattern.push(userChosenColor);
+    animatePress(userChosenColor);
+    checkAnswer(userClickedPattern.length - 1);
+});
+
+function checkAnswer(currentLevel) {
+    if(userClickedPattern[currentLevel] === gamePattern[currentLevel]) {
+        if(userClickedPattern.length === gamePattern.length) {
+            setTimeout(function() {
+                nextSequence();
+            },1000);
+        }
+    } else {
+        $("body").addClass("game-over");
+        playSound("wrong");
+        $("#level-title").text("Game Over, Input your name!");
+        setTimeout(function () {
+            $("body").removeClass("game-over");
+        }, 200);
+
+        const levelInput = document.querySelector('#level');
+        levelInput.value = level;
+        const form2 = document.querySelector('form');
+        form2.removeAttribute('hidden');
+        startOver();
+    }
+}
+function nextSequence() {
+    userClickedPattern = [];
+    level++;
+    $("#level-title").text("Level " + level);
+    let randomNumber = Math.floor(Math.random() * 4);
+    let randomChosenColour = buttonColours[randomNumber];
+    gamePattern.push(randomChosenColour);
+    $("#" + randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
+    playSound(randomChosenColour);
+}
+
+function playSound(name) {
+    let audio = new Audio("sounds/" + name + ".mp3");
+    audio.play();
+}
+
+function animatePress(currentColour) {
+    $("#" + currentColour).addClass("pressed");
+    setTimeout(function() {
+        $("#" + currentColour).removeClass("pressed");
+    }, 100);
+}
+
+function startOver() {
+    level = 0;
+    gamePattern = [];
+    started = false;
+}
